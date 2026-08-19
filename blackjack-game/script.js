a86cf6be2a46250d43bbdd4f6694247c25f1d66e
Roy Bipro <roybipro896@gmail.com>
2026-08-19 20:39:02 +0600

    learn:

--- blackjack-game/script.js
+++ blackjack-game/script.js
@@ -1,9 +1,25 @@
 
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-let firstCard = 10;
-let secondCard = 11;
+let messageEl = document.getElementById("message-el")
 
-let sum = firstCard +secondCard;
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    
+}
 
 
 
